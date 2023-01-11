82f339916e5ff8fecda6842c77486e9d0931fab6
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-01-11 17:06:31 -0300

    Projeto para Curso de Python em videoaulas

---
+++ ola_mundo.py
@@ -0,0 +1,9 @@
+# Surgirá na tela do usuário a mensagem "Olá Mundo!"
+print("Olá Mundo!")
+
+'''
+Este comando print serve para:
+1 - Exibir informações na tela do usuário diretamente de strings (entre aspas)
+2 - Exibir valores que estão salvos em variáveis
+'''
+print("Bom dia!!!")
